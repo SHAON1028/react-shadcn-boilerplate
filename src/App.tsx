e18e5296b43b458ce3e540c4921dd3b59627d027
khairul Islam <khairul.islam@adndiginet.com>
2025-01-29 16:56:37 +0600

    react-ts-shadcn-hook-form-boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <div className='conatiner p-10'>
+      <Button className='cursor-pointer'>Click me</Button>
+    </div>
+  );
+}
+
+export default App
